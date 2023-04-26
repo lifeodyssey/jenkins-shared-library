6c6af6dee2d53ead7bf2f0c2d905167b211ab2ed
lifeodyssey <zhouthepassion@outlook.com>
2023-04-26 15:53:31 +0800

    feat: fix shell issue

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,5 +1,5 @@
-// deployApp.groovy
-def call(String environment, String mongodbUri, String dockerImageName) {
-    def deployment = new org.example.DeployDockerImage(environment, mongodbUri, dockerImageName)
-    deployment.deploy()
-}
+//// deployApp.groovy
+//def call(String environment, String mongodbUri, String dockerImageName) {
+//    def deployment = new org.example.DeployDockerImage(environment, mongodbUri, dockerImageName)
+//    deployment.deploy()
+//}

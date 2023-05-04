824f3020f6a1c717b02b9bb937ac764a98d5a577
lifeodyssey <zhouthepassion@outlook.com>
2023-05-04 14:48:15 +0800

    bugfix:fix load file path issue

--- src/org/example/microservice/config/Utils.groovy
+++ src/org/example/microservice/config/Utils.groovy
@@ -1,6 +1,5 @@
 package org.example.microservice.config
 
-import org.codehaus.groovy.runtime.ResourceGroovyMethods
 import org.example.microservice.config.parameters.Environment
 import org.example.microservice.config.constant.Constant
 class Utils {
@@ -8,7 +7,7 @@ class Utils {
         def environment = Environment.valueOf(env)
         def props = new Properties()
         def classLoader = this.class.classLoader
-        def inputStream = classLoader.getResourceAsStream("${environment.getFilePath()}")
+        def inputStream = classLoader.getResourceAsStream(environment.getFilePath())
         props.load(inputStream)
         return props.getProperty(Constant.MONGODB_URI)
     }

a30450a69968f2b0ffd95e72ed52166130aff2fc
lifeodyssey <zhouthepassion@outlook.com>
2023-05-04 15:23:06 +0800

    feat: add the logic of pipeline

---
+++ vars/buildAndDeployMicroservice.groovy
@@ -0,0 +1,40 @@
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage('Test and Build Jar') {
+                steps {
+                    script {
+                        buildGradle()
+                    }
+                }
+            }
+            stage('Build Image') {
+                steps {
+                    script {
+                        buildDockerImage(imageName: "demo:latest", svcName: "book-svc")
+                    }
+                }
+            }
+            stage('Dev') {
+                steps {
+                    script {
+                        deployApp(APP_ENV: 'DEV')
+                    }
+                }
+            }
+            stage('Deploy to QA approval') {
+                steps {
+                    input "Deploy to QA?"
+                }
+            }K
+            stage('QA') {
+                steps {
+                    script {
+                        deployApp(APP_ENV: 'QA')
+                    }
+                }
+            }
+        }
+    }
+}

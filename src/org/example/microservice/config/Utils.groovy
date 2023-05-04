d2cb1b43c9149b31469be7987b92cd58646c5aa1
lifeodyssey <zhouthepassion@outlook.com>
2023-05-04 15:00:44 +0800

    bugfix:fix load file path issue

--- src/org/example/microservice/config/Utils.groovy
+++ src/org/example/microservice/config/Utils.groovy
@@ -1,14 +1,9 @@
 package org.example.microservice.config
 
 import org.example.microservice.config.parameters.Environment
-import org.example.microservice.config.constant.Constant
 class Utils {
     static String getDatabaseUrlFromResource(String env) {
         def environment = Environment.valueOf(env)
-        def props = new Properties()
-        def classLoader = this.class.classLoader
-        def inputStream = classLoader.getResourceAsStream(environment.getFilePath())
-        props.load(inputStream)
-        return props.getProperty(Constant.MONGODB_URI)
+        return environment.getDBPath()
     }
 }
--- src/org/example/microservice/config/parameters/Environment.groovy
+++ src/org/example/microservice/config/parameters/Environment.groovy
@@ -2,8 +2,8 @@ package org.example.microservice.config.parameters
 
 // Environment.groovy
 enum Environment {
-    DEV("configurations/dev.properties"),
-    QA("configurations/qa.properties"),
+    DEV("mongodb://dev_mongo_username:dev_mongo_pwd@host.docker.internal:27018/?authSource=admin\\\\&tls=false"),
+    QA("mongodb://qa_mongo_username:qa_mongo_pwd@host.docker.internal:27019/?authSource=admin\\\\&tls=false"),
     PROD("configurations/prod.properties")
 
     final String fileName
@@ -12,7 +12,7 @@ enum Environment {
         this.fileName = fileName
     }
 
-    String getFilePath() {
+    String getDBPath() {
         fileName
     }
 }

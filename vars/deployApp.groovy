31ea49a1378e3d3248f52634bfc477b6d29c3a6c
lifeodyssey <zhouthepassion@outlook.com>
2023-05-04 14:17:55 +0800

    bugfix:fix load file path issue

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -4,6 +4,6 @@ import org.example.microservice.config.Utils
 def call(Map config = [:]) {
     loadLinuxScript(name: "deployApp.sh")
     def APP_ENV = config.APP_ENV ?: 'DEV'
-    def MONGODB_URI = config.MONGODB_URI ?: Utils.getDatabaseUrlFromResource(APP_ENV)
+    def MONGODB_URI = Utils.getDatabaseUrlFromResource(APP_ENV)
     sh "./deployApp.sh ${APP_ENV} ${MONGODB_URI}"
 }

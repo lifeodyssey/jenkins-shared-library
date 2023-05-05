4ad41146b70489d65d819ea2596925e3fbd93a80
lifeodyssey <zhouthepassion@outlook.com>
2023-05-05 17:26:40 +0800

    refactor: rewrite use scripted pipeline

--- vars/buildAndDeployMicroservice.groovy
+++ vars/buildAndDeployMicroservice.groovy
@@ -3,42 +3,22 @@ def call(Map config = [:]) {
     String buildTool = config.buildTool
     String imageName = config.imageName
     String svcName = config.svcName
-    pipeline {
-        agent any
-        stages {
-            stage('Test and Build Jar') {
-                steps {
-                    script {
-                        buildArtifact(language, buildTool)
-                    }
-                }
-            }
-            stage('Build Image') {
-                steps {
-                    script {
-                        buildDockerImage(imageName,svcName)
-                    }
-                }
-            }
-            stage('Dev') {
-                steps {
-                    script {
-                        deployApp('DEV')
-                    }
-                }
-            }
-            stage('Deploy to QA approval') {
-                steps {
-                    input "Deploy to QA?"
-                }
-            }
-            stage('QA') {
-                steps {
-                    script {
-                        deployApp('QA')
-                    }
-                }
-            }
+
+    node {
+        stage('Test and Build Jar') {
+            buildArtifact(language, buildTool)
+        }
+        stage('Build Image') {
+            buildDockerImage(imageName, svcName)
+        }
+        stage('Dev') {
+            deployApp('DEV')
+        }
+        stage('Deploy to QA approval') {
+            input "Deploy to QA?"
+        }
+        stage('QA') {
+            deployApp('QA')
         }
     }
 }

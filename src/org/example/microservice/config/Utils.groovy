d2c4b825f9ebd52f3f2e906c59b2a06fa568e45e
lifeodyssey <zhouthepassion@outlook.com>
2023-05-06 15:24:01 +0800

    bugfix:fix how to get db path

--- src/org/example/microservice/config/Utils.groovy
+++ src/org/example/microservice/config/Utils.groovy
@@ -3,7 +3,7 @@ package org.example.microservice.config
 import org.example.microservice.config.parameters.Environment
 class Utils {
     static String getDatabaseUrlFromResource(String env) {
-        def environment = Environment.valueOf(env)
+        def environment = Environment.getEnvironmentByName(env)
         return environment.getDBPath()
     }
 }
--- src/org/example/microservice/config/parameters/Environment.groovy
+++ src/org/example/microservice/config/parameters/Environment.groovy
@@ -6,13 +6,17 @@ enum Environment {
     QA("mongodb://qa_mongo_username:qa_mongo_pwd@host.docker.internal:27019/?authSource=admin\\\\&tls=false"),
     PROD("configurations/prod.properties")
 
-    final String fileName
+    final String dbPath
 
-    Environment(String fileName) {
-        this.fileName = fileName
+    Environment(String dbPath) {
+        this.dbPath = dbPath
     }
 
     String getDBPath() {
-        fileName
+        dbPath
     }
+    static Environment getEnvironmentByName(String name) {
+        Environment.values().find { it.name() == name }
+    }
+
 }
